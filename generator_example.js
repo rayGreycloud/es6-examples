9c9d5c7101cdcadf4854447da50b2292122bebe4
rayGreycloud <rayGreycloud@gmail.com>
2016-08-02 14:18:29 -0700

    first commit

---
+++ generator_example.js
@@ -0,0 +1,30 @@
+// ES6 Generator example
+
+// object
+const engineeringTeam = {
+  size: 3,
+  department: 'Engineering',
+  lead: 'Jill',
+  manager: 'Alex',
+  engineer: 'Dave'
+};
+
+// generator that allows for iteration over select events
+function *TeamIterator(team) {
+  // first value that will be provided to for of loop
+  yield team.lead;
+  // second
+  yield team.manager;
+  // third
+  yield team.engineer;
+}
+// initialize result array
+const names = [];
+
+// iterate over each yield value from generator
+
+for (let name of TeamIterator(engineeringTeam)) {
+  names.push(name);
+}
+
+console.log(names);
