fe34e4b668065804d4d929545c57bc1be1d13947
rayGreycloud <rayGreycloud@gmail.com>
2016-08-03 12:29:05 -0700

    Add exercise 34 recursion with destructuring

---
+++ exercise34_recursionWithDestructuring.js
@@ -0,0 +1,37 @@
+//From Stephen Grider's ES6 Javascript course on udemy.com
+// Coding Exercise 34: Recursion with Destructuring
+
+//This one is probably the hardest exercise in the entire course!
+// Use array destructuring, recursion, and the rest/spread operators to
+// create a function 'double' that will return a new array with all values
+// inside of it multiplied by two.  Do not use any array helpers!
+// double([1,2,3]) should output [2,4,6]
+
+const numbers = [1, 2, 3];
+
+// one solution
+function double(arrayOfNumbers) {
+    const [number, ...rest] = arrayOfNumbers;
+
+    if (rest.length) {
+        return [number * 2, ...double(rest)];
+    } else {
+        return [number * 2];
+    }
+}
+console.log(double(numbers)) // expect [2,4,6]
+
+// my solution
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+function doubled(arrayOfNumbers) {
+    const [number, ...rest] = arrayOfNumbers;
+
+   if (arrayOfNumbers.length === 0) {
+      return number * 2;
+    } else {
+  		return [number * 2, ...doubled(rest)];
+    }
+}
+console.log(doubled(numbers)); // expect [2,4,6,8,10,12]
